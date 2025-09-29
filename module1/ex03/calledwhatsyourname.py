cf1d913d476cf538ca71ff895ff0d99d31077f56
lalves-d <l.v.alvesjr@gmail.com>
2025-09-29 11:38:55 -0300

    module1

---
+++ module1/ex03/calledwhatsyourname.py
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    calledwhatsyourname.py                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lalves-d <lalves-d@student.42.rio>         +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/09/29 11:28:12 by lalves-d          #+#    #+#              #
+#    Updated: 2025/09/29 11:36:35 by lalves-d         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+whole_name = input ("Hey, what's your first name? : ")
+whole_name += " "
+whole_name += input ("And your last name? : ")
+
+print("Well, pleased to meet you,", whole_name)
