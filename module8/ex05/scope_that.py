543ce135e9bebfe0ec603741f3388240704b93f8
LuizVitorAlves <l.v.alvesjr@gmail.com>
2025-10-02 23:23:46 -0300

    module8 correção

--- module8/ex05/scope_that.py
+++ module8/ex05/scope_that.py
@@ -11,8 +11,8 @@ try:
     print("Before function", num)
     add_one(num)
     print("After function:", num)
-    change_num = add_one(num)
-    print("Changed variable:", change_num)
+    #change_num = add_one(num)
+    #print("Changed variable:", change_num)
 
 except:
     print("Error")
